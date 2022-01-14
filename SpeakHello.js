2a96970425b4e41b119f5e90f0806dcabd65dac1
fernando <ferbrice@hotmail.com>
2022-01-14 07:19:28 -0500

    Fixing again

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,9 +1,9 @@
 (function (window){
   var speakWord = "Hello";
   var helloSpeaker = {};
-  speakWord.speak = function (name) {
+  helloSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
   }
-  window.speakWord=speakWord;
+  window.helloSpeaker=helloSpeaker;
 })(window);
 
